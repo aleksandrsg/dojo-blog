eb908b5f83907938f3aaa75cb73f25806fb71827
aleksandrsg <aleksandrs.galiullins@gmail.com>
2021-07-25 19:43:44 +0100

    Use .map method through blogs

--- src/Home.js
+++ src/Home.js
@@ -2,19 +2,25 @@ import {useState} from 'react';
 
 
 const Home = () => {
-    // let name = 'Aleks';
-    const [name, setName] = useState('Olga');
-    const [age, setAge] = useState(39);
-    const handleClick = () => {
-        setName('Lada');
-        setAge(5);
-    }
+
+    const [ blogs, setBlogs] = useState (
+        [
+            { title: "My first React App", body: 'lorem ipsum...', author: "Aleks", id: 1},
+            { title: "Junior web developer", body: 'lorem ipsum...', author: "Aleks", id: 2},
+            { title: "Coding for dummies", body: 'lorem ipsum...', author: "Aleks", id: 3},
+        ]
+    );
 
     return (  
         <div className="home">
-            <h2>Homepage</h2>
-            <p> {name} + {age} </p>
-            <button onClick={handleClick} >Click Me</button>
+            {blogs.map((blog) => (
+                <div className="blog-preview">
+                    <h2>{blog.title}</h2>
+                    <p> {blog.author}</p>
+                    <p> {blog.body}</p>
+                </div>
+            ))
+            }
         </div>
     );
 }
