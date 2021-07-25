7c998dd52693973a207e14ce455776ab8ec013ce
aleksandrsg <aleksandrs.galiullins@gmail.com>
2021-07-25 20:27:33 +0100

    Reusing BlogList Component  with filter method

--- src/Home.js
+++ src/Home.js
@@ -7,14 +7,15 @@ const Home = () => {
     const [ blogs, setBlogs] = useState (
         [
             { title: "My first React App", body: 'lorem ipsum...', author: "Aleks", id: 1},
-            { title: "Junior web developer", body: 'lorem ipsum...', author: "Aleks", id: 2},
-            { title: "Coding for dummies", body: 'lorem ipsum...', author: "Aleks", id: 3},
+            { title: "Junior web developer", body: 'lorem ipsum...', author: "Olga", id: 2},
+            { title: "Coding for dummies", body: 'lorem ipsum...', author: "Lada", id: 3},
         ]
     );
 
     return (  
         <div className="home">
             < BlogList blogs = { blogs} title = "All blogs!"/>
+            < BlogList blogs = { blogs.filter((blog) => blog.author === 'Aleks')} title = "Aleks Blogs"/>
         </div>
     );
 }
