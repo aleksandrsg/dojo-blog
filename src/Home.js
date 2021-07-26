2820a1e15e71a812693b3b501824be9260e3f0c5
aleksandrsg <aleksandrs.galiullins@gmail.com>
2021-07-26 19:16:43 +0100

    useEffect dependency (name)

--- src/Home.js
+++ src/Home.js
@@ -17,14 +17,18 @@ const Home = () => {
         setBlogs(updatedBlogs);
     }
 
+    const [name, setName ] = useState('Olga');
+
     useEffect( () => {
         console.log ('Rerendered')
-    });
+    }, [name]);
 
     return (  
         <div className="home">
             < BlogList blogs = { blogs} title = "All blogs!" deleteBlog= {deleteBlog}/>
             < BlogList blogs = { blogs.filter((blog) => blog.author === 'Aleks')} title = "Aleks Blogs" deleteBlog= {deleteBlog}/>
+            <p> {name}</p>
+            <button onClick ={ () => {setName('Aleks')} }> Change name</button>
         </div>
     );
 }
