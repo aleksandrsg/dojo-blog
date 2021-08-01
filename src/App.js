096451a5a29c45c20cc4f9e4c25ff067a711252e
aleksandrsg <aleksandrs.galiullins@gmail.com>
2021-08-01 14:10:46 +0100

    Import Link Component

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './index.css';
 import Navbar from './Navbar.js';
 import Home from './Home.js';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Create from './Create';
 
 const App = () => {
 
@@ -11,8 +12,11 @@ const App = () => {
         <Navbar />
       <div className="content">
         <Switch>
-          <Route path='/'>
+          <Route exact path='/'>
             <Home />
+          </Route>
+          <Route path='/create'>
+            <Create />
           </Route>
         </Switch>
       </div>
