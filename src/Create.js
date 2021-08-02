b5f1eeac072c472bb837a4bddb5d4fa6d70899e3
aleksandrsg <aleksandrs.galiullins@gmail.com>
2021-08-02 18:03:46 +0100

    Use useHistory hook for redirection to Home page

--- src/Create.js
+++ src/Create.js
@@ -1,15 +1,32 @@
 import { useState } from "react";
+import { useHistory } from "react-router";
+
 
 const Create = () => {
 
     const [title, setTitle] = useState('');
     const [body, setBody] = useState('');
     const [author, setAuthor] = useState('Olga');
+    const history = useHistory();
+
+    const handleSubmit = (e) =>{
+        e.preventDefault();
+        const blog = { title, body, author};
+
+        fetch('http://localhost:8000/blogs/', {
+            method: 'POST',
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify(blog)
+          }).then(() => {
+            console.log('new blog added');
+            history.push('/');
+          })
+    }
 
     return (  
         <div className="create">
             <h2>Add New Blog</h2>
-            <form>
+            <form onSubmit = {handleSubmit}>
                 <label> Blog Title:</label>
                 <input type="text" 
                 required
