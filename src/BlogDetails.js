b965400e381d2c37dae8a8898a0eb09071ce1b16
aleksandrsg <aleksandrs.galiullins@gmail.com>
2021-08-02 18:16:16 +0100

    Delete Blog

--- src/BlogDetails.js
+++ src/BlogDetails.js
@@ -1,4 +1,4 @@
-import { useParams } from "react-router-dom";
+import { useParams, useHistory } from "react-router-dom";
 import useFetch from "./useFetch";
 
 const BlogDetails= () => {
@@ -6,6 +6,15 @@ const BlogDetails= () => {
     const {id } = useParams();
 
     const {data:blog, isLoading, error } =useFetch('http://localhost:8000/blogs/' + id )
+    const history = useHistory();
+
+    const deleteBlog = () => {
+    fetch('http://localhost:8000/blogs/' + blog.id, {
+      method: 'DELETE'
+    }).then(() => {
+      history.push('/');
+    }) 
+    }
 
     return ( 
         <div className="blog-details">
@@ -17,6 +26,7 @@ const BlogDetails= () => {
                         <h2>{blog.title}</h2>
                         <p>Written by {blog.author}</p>
                         <div> {blog.body}</div>
+                        <button onClick={deleteBlog}>Delete Blog</button>
                     </article>
                 )}
         </div>
